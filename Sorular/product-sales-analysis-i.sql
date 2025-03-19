d8378653e10572b57699cd1b6ce72beb1ed16e5a
ErdemAkgul <77586895+ErdemAkgul@users.noreply.github.com>
2025-03-20 02:04:38 +0300

    Create product-sales-analysis-i

---
+++ Sorular/product-sales-analysis-i.sql
@@ -0,0 +1,34 @@
+/*
+url: https://leetcode.com/problems/product-sales-analysis-i/?envType=study-plan-v2&envId=top-sql-50
+
+Input: 
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+Product table:
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+| 100        | Nokia        |
+| 200        | Apple        |
+| 300        | Samsung      |
++------------+--------------+
+Output: 
++--------------+-------+-------+
+| product_name | year  | price |
++--------------+-------+-------+
+| Nokia        | 2008  | 5000  |
+| Nokia        | 2009  | 5000  |
+| Apple        | 2011  | 9000  |
++--------------+-------+-------+
+*/
+
+select p.product_name,s.year, s.price
+from Sales s
+INNER JOIN Product p on s.product_id = p.product_id
+order by p.product_name, s.year;
